9bf198f436cba10b5efe67f055bc32e09e5ce683
Naveen <akula.veera2023@vitstudent.ac.in>
2026-02-19 14:35:36 +0530

    Remove unnecessary comments from App.java
    Removed commented instructions for AppTest.java.

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -4,15 +4,3 @@ public int add(int a, int b) {
 return a + b;
 }
 }
-COMMIT THE CHANGES
-FOR THE AppTest.JAVA use the FOLLOWING CODE:
-package com.example;
-import org.junit.Test;
-import static org.junit.Assert.*;
-public class AppTest {
-@Test
-public void testAdd() {
-App app = new App();
-assertEquals(5, app.add(2, 3));
-}
-}
